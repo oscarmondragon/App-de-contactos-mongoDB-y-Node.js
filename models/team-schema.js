ad1dd1b7d12c4a9bc8df8342d76285f06395b49b
Oscar <oscarmondragon100@gmail.com>
2017-04-11 23:23:42 -0500

    Se agregaron validaciones
    Se  implementaron en la parte del servidor en  telefono y email del
    usuario en el archivo team-schema.js

--- models/team-schema.js
+++ models/team-schema.js
@@ -1,17 +1,24 @@
 'use strict';
-
+var email_match = [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Coloca un email valido"];
 const mongoose = require('./model'),
-	Schema = mongoose.Schema,
-	TeamSchema = new Schema({
-		_id : Schema.Types.ObjectId,
-		name : String,
-		phone : String,
-		email : String,
-		facebook : String
-	},
-	{
-		collection : 'contactos'
-	}),
-	Team = mongoose.model('Contactos', TeamSchema);
-
+    Schema = mongoose.Schema,
+    TeamSchema = new Schema({
+        _id: Schema.Types.ObjectId,
+        name: String,
+        phone: {
+            type: String,
+            required: true,
+            maxlength: [11, "Telefono muy  grande "],
+            minlength: [10, "Debe tener minimo 10 digitos"]
+        },
+        email: {
+            type: String,
+            required: "El correo es obligatorio",
+            match: email_match
+        },
+        facebook: String
+    }, {
+        collection: 'contactos'
+    }),
+    Team = mongoose.model('Contactos', TeamSchema);
 module.exports = Team;
